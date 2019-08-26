174d203f63ffb1bff2a7da766b70fe766bac89aa
BarcaBro1022 <you.k2208@gmail.com>
2019-08-25 18:59:25 -0700

    added comments to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,8 @@
+// letters array
+
 const letters = [ `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z` ]
+
+// select random letter 
 
 const randomLetter = letters[Math.floor(Math.random()*letters.length)]
 
