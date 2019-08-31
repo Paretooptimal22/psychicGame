5c5419ee8280d9d39f0e22d1346f99f052e90dff
BarcaBro1022 <you.k2208@gmail.com>
2019-08-30 20:29:28 -0700

    added  tracking variables and keyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,15 @@ const letters = [ `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m
 const randomLetter = letters[Math.floor(Math.random()*letters.length)]
 
 console.log(randomLetter)
+
+// wins/losses/guesses tracker
+
+let wins = 0
+let losses = 0
+let guesses = 9
+
+// player input event
+
+document.onkeyup = function (event) {
+  console.log(event.key)
+}
