983268a9c92f4d3ea6ff4f44c433ed082237a3dd
BarcaBro1022 <you.k2208@gmail.com>
2019-08-30 21:12:24 -0700

    append trackers to <p> ids

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,12 +14,16 @@ let wins = 0
 let losses = 0
 let guesses = 9
 
+document.getElementById(`wins`).append(` ` + wins)
+document.getElementById(`losses`).append(` ` + losses)
+document.getElementById(`guessesLeft`).append(` ` + guesses)
+
 // player input event - letter interpretation
 
 const guess = document.onkeyup = function (event) {
   if (event.keyCode >= 65 && event.keyCode <= 90) {
     console.log(event.key) 
-    document.getElementById(`soFar`).append(event.key)
+    document.getElementById(`soFar`).append(` ` + event.key)
   }
   
 }
