1c1e382ceee767f23d477cfdd9ff6345382435d4
BarcaBro1022 <you.k2208@gmail.com>
2019-09-02 18:33:45 -0700

    reorganized javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,4 @@
-// letters array
-
-const letters = [ `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z` ]
-
-
-// select random letter 
-
-const randomLetter = letters[Math.floor(Math.random()*letters.length)]
-
-console.log(randomLetter)
-
-
-
-
 // wins/losses/guesses tracker
-
 let wins = 0
 let losses = 0
 let guesses = 9
@@ -24,15 +9,26 @@ document.getElementById(`losses`).innerHTML = `Losses: ` + losses
 
 document.getElementById(`guessesLeft`).innerHTML = `Guesses Left: ` + guesses
 
-// player input event - letter interpretation
 
+// letters array
+const letters = [ `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z` ]
+
+
+// select random letter 
+const randomLetter = () => {
+  return letters[Math.floor(Math.random()*letters.length)]
+}
+
+
+// player input event - letter interpretation
 document.onkeyup = (event) => {
   if (event.keyCode >= 65 && event.keyCode <= 90) {
     console.log(event.key) 
-
-    guesses--
     
     document.getElementById(`soFar`).append(` ` + event.key)
+
+    // win: player guess = randomLetter
+    
 
     
   }
@@ -41,6 +37,8 @@ document.onkeyup = (event) => {
 
 
 // conditionals
+
+
 
 
 
