bc904be5d4e2bff2ef8d23601bfc883a3118e8f4
BarcaBro1022 <you.k2208@gmail.com>
2019-09-02 19:36:16 -0700

    changeDisplay function  & win condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ let wins = 0
 let losses = 0
 let guesses = 9
 
-document.getElementById(`wins`).innerHTML = `Wins: ` + wins
+// document.getElementById(`wins`).innerHTML = `Wins: ` + wins
 
-document.getElementById(`losses`).innerHTML = `Losses: ` + losses
+// document.getElementById(`losses`).innerHTML = `Losses: ` + losses
 
-document.getElementById(`guessesLeft`).innerHTML = `Guesses Left: ` + guesses
+// document.getElementById(`guessesLeft`).innerHTML = `Guesses Left: ` + guesses
 
 
 // letters array
@@ -19,16 +19,34 @@ const randomLetter = () => {
   return letters[Math.floor(Math.random()*letters.length)]
 }
 
+const compLetter = randomLetter()
+console.log(compLetter)
+
+const changeDisplay = () => {
+  document.getElementById(`display`).innerHTML = `
+  <h2>Guess what letter I am thinking of</h2>
+  <h3>Wins: ${wins}</h3>
+  <h3>Losses: ${losses}</h3>
+  <h3>Guesses left: ${guesses}</h3>
+  <h3>Your guesses so far:</h3>
+  `
+}
+changeDisplay()
 
 // player input event - letter interpretation
 document.onkeyup = (event) => {
   if (event.keyCode >= 65 && event.keyCode <= 90) {
     console.log(event.key) 
+
+    changeDisplay()
     
-    document.getElementById(`soFar`).append(` ` + event.key)
+    // document.getElementById(`soFar`).append(` ` + event.key)
+
 
     // win: player guess = randomLetter
-    
+    if (event.key === compLetter) {
+      wins++
+    }
 
     
   }
